4c9487248cfb206ca0ab9e509a7cbbe35e0e1f4f
Jordan Ricarte <jordan@ricarte.net>
2019-11-04 23:35:49 -0600

    game logic works with user click input

--- my-app/src/components/Banner.js
+++ my-app/src/components/Banner.js
@@ -1,21 +1,13 @@
 import React from 'react';
 
-const style = {
-  logoIcon: {
-    width: 150,
-    heigth: 100
-  },
-  bgDark: {
-    backgroundColor: `rgb(1,0,1)`
-  }
-}
-
 const Banner = props => (
-  <article style={style.bgDark} className="mw7 center ph3 ph5-ns tc br2 pv3 bg-black-90 mb5 animated fadeIn">
-    <img style={style.logoIcon} className='logoIcon' src="./img/pacman.gif" alt="pacman"/>
-    <h2 className="fw2 f4 lh-copy mt0 mb3 white animated fadeIn slower">
-    Click on an image to earn points, but don't click on any more than once!
-    </h2>
-  </article>
+  <div>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Clicky</h1>
+        <p class="lead">A memory game for kids!</p>
+      </div>
+    </div>
+  </div>
 );
 export default Banner;
--- my-app/src/components/IconCard.js
+++ my-app/src/components/IconCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+const style = {
+    imgResize: {
+        width: 150,
+        heigth: 100,
+    }
+}
 
 const IconCard = props => (
-  <section id={props.id} className="tc pa3 hvr-float-shadow animated zoomInUp" value={props.id} onClick={() =>
+  <section  id={props.id} value={props.id} onClick={() =>
   props.clickedPlayer(props.id)
   }>
-    <article className="w4 pa2 ">
-      <img src={props.image} className="db" alt="game-char" />
+    <article>
+      <img style={style.imgResize} src={props.image} alt="game-char" />
     </article>
   </section>
 )
